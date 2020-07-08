c634e1fe088d4dc389f233e845806fd3164db1c2
Stephen Harnois <stephen.harnois@live.com>
2020-07-08 16:27:56 -0400

    add if statement

--- hello.rb
+++ hello.rb
@@ -1,8 +1,7 @@
-# Lesson 1: Outputting a String
+#!/usr/bin/ruby
 
-puts "What is your name? "
-
-# Lesson 2: Prompting User Input
-
-name = gets.chop
-puts "Hey, #{name}! Nice to meet you!"
+puts "What is your favorite color? "
+color = gets.chop
+if color == "blue"
+    puts "Wow! My favorite color is #{color} also!"
+end
